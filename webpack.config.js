122d553c0ff4d219652f9aa05bbdec4bddae229a
Muhammadamin <muhamadamin1992@gmail.com>
2018-03-13 12:45:01 +0500

    Polling instead nodemon but with error

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,14 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-
+const path = require('path');
+const webpack = require('webpack');
 module.exports = {
 
     devServer: {
+        watchOptions: {
+            aggregateTimeout: 300,
+            poll: 1000
+        },
+
         stats: "errors-only",
         host: process.env.HOST,
         port: process.env.PORT,
@@ -14,6 +20,10 @@ module.exports = {
     },
 
     plugins: [
+        new webpack.WatchIgnorePlugin([
+            path.join(__dirname, "node_modules")
+        ]),
+
         new HtmlWebpackPlugin({
             title: "Webpack demo"
         })
