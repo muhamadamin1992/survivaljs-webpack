300b020b6bf7e0f6337214bee133b1694143ae5f
Muhammadamin <muhamadamin1992@gmail.com>
2018-06-14 15:43:09 +0500

    use nosources-source-map

--- webpack.config.js
+++ webpack.config.js
@@ -24,7 +24,7 @@ const commonConfig = merge([
 const productionConfig = merge([
 
     parts.generateSourceMaps({
-        type: "hidden-source-map"
+        type: "nosources-source-map"
     }),
 
     parts.extractCSS({
