2be577ef1ed74ca5dc4b93a706a0c9ad401450a3
Muhammadamin <muhamadamin1992@gmail.com>
2018-06-14 14:55:10 +0500

    use cheap-module-source-map

--- webpack.config.js
+++ webpack.config.js
@@ -24,7 +24,7 @@ const commonConfig = merge([
 const productionConfig = merge([
 
     parts.generateSourceMaps({
-        type: "cheap-source-map"
+        type: "cheap-module-source-map"
     }),
 
     parts.extractCSS({
