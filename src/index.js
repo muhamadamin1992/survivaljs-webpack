7b2fdc6adc86bc078e6b5f3edd8f691e2ea6a3eb
Cvdeby Coryphaeus <coryph13@gmail.com>
2018-10-04 14:34:12 +0500

    Demonstrating Tree shaking

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import "react-dom";
 import "purecss";
 import "./main.css";
 import component from "./component";
+import { bake } from "./shake";
+
+bake();
 
 document.body.appendChild(component());
 class exteb {
